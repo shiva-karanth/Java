676d7142a2c196fca838f368b42aa4d7ff3d4775
Paramashiva Karanth <86004162+shiva-karanth@users.noreply.github.com>
2022-10-21 03:58:45 +0530

    Update helloWorld.java
    modification in gimkunder
    Applied Thread feature

--- Jim Kundar/helloWorld.java
+++ Jim Kundar/helloWorld.java
@@ -1,5 +1,26 @@
-class helloWorld{
+public class Main{
     public static void main(String[] args){
-        System.out.println("Hello World!");
+        System.out.println("Hello guys");
+        try {
+        Thread.sleep(200);
+        } catch(Exception e) {
+            System.out.print("Ah Snap");
+        }
+        System.out.print("I am gym Kundar ");
+         try {
+        Thread.sleep(200);
+        } catch(Exception e) {
+            System.out.print("Ah Snap");
+        }
+        System.out.print("💪 ");
+        for(int i=0;i<3;i++)
+        {
+        System.out.print("."); try {
+        Thread.sleep(300);
+        } catch(Exception e) {
+            System.out.print("Ah Snap");
+        }
+        }
+        System.out.print("!");
     }
 }
